456be95b73dc328dfbb9449ecda618cfc342dfa5
bonot9434 <bonot9434@gmail.com>
2021-11-04 13:34:04 +0000

    Post編集

--- test/system/posts_test.rb
+++ test/system/posts_test.rb
@@ -14,6 +14,8 @@ class PostsTest < ApplicationSystemTestCase
     visit posts_url
     click_on "New Post"
 
+    fill_in "Content", with: @post.content
+    fill_in "Title", with: @post.title
     click_on "Create Post"
 
     assert_text "Post was successfully created"
@@ -24,6 +26,8 @@ class PostsTest < ApplicationSystemTestCase
     visit posts_url
     click_on "Edit", match: :first
 
+    fill_in "Content", with: @post.content
+    fill_in "Title", with: @post.title
     click_on "Update Post"
 
     assert_text "Post was successfully updated"
