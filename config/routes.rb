2357fed1b3d1050dd9f85ae77fe3d3201e8bc2e8
bonot9434 <bonot9434@gmail.com>
2021-11-04 14:02:41 +0000

    いいね機能更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'favorites/create'
-  get 'favorites/destroy'
-  resources :posts
   devise_for :users
+
+  resources :posts do
+    resource:favorites,only:[:create,:destroy]
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
